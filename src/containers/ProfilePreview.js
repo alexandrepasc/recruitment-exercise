4e0e901dd95d89324cdb8058e1d6ba1c5f3cca10
Alexandre Pascoal <apascoal@ubiwhere.com>
2020-11-07 16:19:05 +0000

    Add profile preview container

---
+++ src/containers/ProfilePreview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { RenderProfile } from '../impl/ProfilePreviewImpl';
+
+export default function ProfilePreview(props) {
+
+    return (
+        <Row className='justify-content-md-center'>
+            <Col className='col-md-12'>
+                <NavLink to='/' activeClassName='RegisterUserForm'>
+                    Back
+                </NavLink><br/>
+                <Row><Col>&nbsp;</Col></Row>
+                <Row><Col>{RenderProfile(props.profile)}</Col></Row>
+            </Col>
+        </Row>
+        
+    );
+}
